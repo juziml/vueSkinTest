51d34c925dc96e8e0f031927b105bc6875631d0f
zhushiwen <sink50@foxmail.com>
2023-02-01 17:56:01 +0800

    输入组件，状态记录

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input">What needs to be done?</label>
+    <ul>
+      <li>
+        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model="label"/>
+        <button type="submit">Add</button>
+      </li>
+    </ul>
+
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+  methods: {
+    onSubmit() {
+      console.log("form submitted----:", this.label)
+    }
+  },
+  data() {
+    return {
+      label: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
