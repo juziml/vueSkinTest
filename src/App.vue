3690a239978330ac3d614f044dc2ee9f5fc79df4
zhushiwen <sink50@foxmail.com>
2023-02-01 16:40:59 +0800

    单组件创建

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
-<script setup lang="ts">
+<script>
+import uniqueId from "lodash.uniqueid";
 import TodoItem from './components/ToDoItem.vue'
+import ToDoForm from "./components/ToDoForm.vue";
 
+export default {
+  name: 'app',
+  components: {
+    TodoItem,
+    ToDoForm
+  },
+  data() {
+    return {
+      TodoItems: [
+        {id: uniqueId('todo-'), label: 'Learn Vue1', done: false},
+        {id: uniqueId('todo-'), label: 'Learn Vue2', done: true},
+        {id: uniqueId('todo-'), label: 'Learn Vue3', done: true},
+        {id: uniqueId('todo-'), label: 'Learn Vue4', done: false},
+      ]
+    }
+  }
+}
 
 
 </script>
@@ -10,13 +29,13 @@ import TodoItem from './components/ToDoItem.vue'
     <h1>To-Do List</h1>
     <ul>
       <li>
-        <todo-item label="第三"></todo-item>
+        <ToDoForm></ToDoForm>
       </li>
-      <li>
-        <todo-item label="第三"></todo-item>
+      <li v-for="item in TodoItems" :key="item.id">
+        <TodoItem :label="item.label" :id="item.id" :done="item.done"></TodoItem>
       </li>
-      <li>
-        <todo-item label="第三4"></todo-item>
+      <li v-for="item in TodoItems" :key="item.id">
+        <todo-item :label="item.label" :id="item.id" :done="item.done"></todo-item>
       </li>
     </ul>
   </div>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
-    <input type="checkbox" id="todo-item" checked="false">
-    <label for="todo-item">{{ label }}</label>
+    <!--    v-bind: 可缩写为 :-->
+    <input type="checkbox" :id="id" :checked="isDone"/>
+    <label :for="id">{{ label }}</label>
 
   </div>
 
 </template>
 
 <script>
+import uniqueId from 'lodash.uniqueid'
+
 export default {
   name: "ToDoItem",
   props: {
+    id: {required: true, type: String},
     label: {required: true, type: String},
     done: {default: false, type: Boolean}
+  },
+  data() {
+    return {
+      isDone: this.done,
+    };
   }
 }
 </script>
